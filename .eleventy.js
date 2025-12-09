b91076a0bbe13e0ef385f71cb8a5db5fa7f48847
phgwiki <nstotskaya@gmail.com>
2025-12-09 21:11:56 +0100

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -2,8 +2,21 @@ const markdownIt = require("markdown-it");
 const markdownItFootnote = require("markdown-it-footnote");
 
 module.exports = function(eleventyConfig) {
-  const mdLib = markdownIt({ html: true, linkify: true })
-    .use(markdownItFootnote);
+  const mdLib = markdownIt({
+    html: true,
+    linkify: true,
+    typographer: true,
+  }).use(markdownItFootnote);
 
   eleventyConfig.setLibrary("md", mdLib);
+
+  // ВАЖНО: сказать Eleventy, где лежит контент и инклюды
+  return {
+    dir: {
+      input: "src",        // твой контент: ch1.md, intro1.md и т.п.
+      includes: "../_includes", // твои layout'ы лежат в корне
+      data: "../_data",         // данные тоже в корне
+      output: "_site"
+    }
+  };
 };
